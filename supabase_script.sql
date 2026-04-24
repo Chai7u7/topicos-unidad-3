96d8f81c01df896d664b116c988ff7f46854403d
Chai7u7 <shai070674@gmail.com>
2026-04-24 12:16:25 -0600

    Remove SQL script for adding new columns to 'usuarios' table in Supabase

--- supabase_script.sql
+++
@@ -1,8 +0,0 @@
--- Script SQL para agregar las nuevas columnas a la tabla 'usuarios' en Supabase
--- Ejecuta este script en el SQL Editor de tu proyecto Supabase
-
-ALTER TABLE usuarios ADD COLUMN nombre TEXT;
-ALTER TABLE usuarios ADD COLUMN apellido_paterno TEXT;
-ALTER TABLE usuarios ADD COLUMN apellido_materno TEXT;
-ALTER TABLE usuarios ADD COLUMN edad INTEGER;
-ALTER TABLE usuarios ADD COLUMN pais TEXT;
